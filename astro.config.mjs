fc3c065b10b3af7bdba8227ea15616f83bf6ec70
jpgcano <juan-0395@hotmail.com>
2024-04-20 21:24:50 -0500

    preparando configuracion vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
+  output: 'static',
+  adapter: vercel({
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+    },
+  }),
 });
